bc3b52bb474ae959705088aabef87ac3360cbf13
Renaud Jenny <renaud.jenny@gmail.com>
2020-04-18 14:54:56 +0100

    tests: add and apply Lint rules for tests as well

--- Tests/SwiftPastTenTests/XCTestManifests.swift
+++ Tests/SwiftPastTenTests/XCTestManifests.swift
@@ -2,9 +2,9 @@ import XCTest
 
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
-    return [
+    [
         testCase(SwiftPastTenTests.allTests),
-        testCase(Utilities.allTests),
+        testCase(Utilities.allTests)
     ]
 }
 #endif
